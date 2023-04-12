ec8b1901ba699f853b0e9924fab3bb3e89cbdf39
Frank Luan <lsf@berkeley.edu>
2023-04-12 00:05:11 +0000

    Wrap error page with AppLayout

--- app/layout/RootError.tsx
+++ app/layout/RootError.tsx
@@ -1,27 +1,36 @@
 /* SPDX-FileCopyrightText: 2020-present Kriasoft */
 /* SPDX-License-Identifier: MIT */
 
-import { Container, Typography } from "@mui/material";
+import { Container, Toolbar, Typography } from "@mui/material";
 import { useRouteError } from "react-router-dom";
+import { AppFooter } from "./components/AppFooter.js";
+import { AppToolbar } from "./components/AppToolbar.js";
 
 export function RootError(): JSX.Element {
   const err = useRouteError() as RouteError;
 
   return (
-    <Container sx={{ marginTop: "43vh" }} maxWidth="sm">
-      <Typography
-        sx={{
-          fontSize: "2em",
-          fontWeight: 300,
-          "& strong": { fontWeight: 400 },
-        }}
-        variant="h1"
-        align="center"
-      >
-        <strong>Error {err.status || 500}</strong>:{" "}
-        {err.statusText ?? err.message}
-      </Typography>
-    </Container>
+    <>
+      <AppToolbar />
+      <Toolbar />
+
+      <Container sx={{ marginTop: "25vh" }} maxWidth="sm">
+        <Typography
+          sx={{
+            fontSize: "2em",
+            fontWeight: 300,
+            "& strong": { fontWeight: 400 },
+          }}
+          variant="h1"
+          align="center"
+        >
+          <strong>Error{err.status ? err.status + " " : null}</strong>:{" "}
+          {err.statusText ?? err.message}
+        </Typography>
+      </Container>
+
+      <AppFooter />
+    </>
   );
 }
 
