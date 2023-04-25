03d8ce96f53bb5cde97d74904778b5986a592ebf
Frank Luan <lsf@berkeley.edu>
2023-04-25 06:45:33 +0000

    Embed typeform

--- app/routes/custom-request/CustomRequestPage.tsx
+++ app/routes/custom-request/CustomRequestPage.tsx
@@ -2,14 +2,37 @@
 /* SPDX-License-Identifier: MIT */
 
 import { Container, Typography } from "@mui/material";
+import { Widget } from "@typeform/embed-react";
 import { usePageEffect } from "../../core/page.js";
 
 export default function CustomRequestPage(): JSX.Element {
-  usePageEffect({ title: "Custom Sourcing Request" });
+  usePageEffect({ title: "White-Glove VIP Service" });
 
   return (
-    <Container>
-      <Typography variant="h1">Custom Request</Typography>
+    <Container maxWidth="md" sx={{ mt: 6, mb: 6 }}>
+      <Typography variant="h1" mb={2}>
+        White-Glove VIP Service
+      </Typography>
+      <Typography variant="body1" mb={1}>
+        Our agency provides white-glove service for you to launch custom
+        products - we do everything from design, sampling, production, packaging
+        and quality control, to global fulfillment.
+      </Typography>
+      <Typography variant="body1">
+        White-glove VIP service MOQ starts at 1,000 units.
+      </Typography>
+      <Typography variant="h2" mt={4} mb={2}>
+        New Client? Fill out this form first:
+      </Typography>
+      <Widget id="djEwoPSB" style={{ height: 600 }} />
+      <Typography variant="h2" mt={4} mb={2}>
+        Submit a Custom Product Sourcing Request
+      </Typography>
+      <Typography variant="body1" mb={2}>
+        Limit one form to one custom product submission. If you are looking to
+        source multiple products, please fill out this form multiple times.
+      </Typography>
+      <Widget id="GU8mFH90" style={{ height: 600 }} />
     </Container>
   );
 }
