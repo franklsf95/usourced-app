33f7108b425ceb3e07a6ad8e64029ef04b6afcdb
Frank Luan <lsf@berkeley.edu>
2023-05-02 07:46:34 +0000

    Demo banner

--- app/layout/DemoLayout.tsx
+++ app/layout/DemoLayout.tsx
@@ -1,9 +1,28 @@
 /* SPDX-FileCopyrightText: 2014-present Kriasoft */
 /* SPDX-License-Identifier: MIT */
 
+import { Box, Container, Typography } from "@mui/material";
 import * as React from "react";
 import { Outlet } from "react-router-dom";
 import { DemoToolbar } from "./components/DemoToolbar.js";
+
+function DemoBanner(): JSX.Element {
+  return (
+    <Box sx={{ backgroundColor: "#183439", height: 32, py: 1, mt: 12 }}>
+      <Container>
+        <Typography
+          variant="h1"
+          fontSize={16}
+          textTransform="uppercase"
+          color="white"
+          align="center"
+        >
+          Demo site &ndash; we are launching in Q3 2023
+        </Typography>
+      </Container>
+    </Box>
+  );
+}
 
 /**
  * The primary application layout.
@@ -12,7 +31,7 @@ export function DemoLayout(): JSX.Element {
   return (
     <React.Fragment>
       <DemoToolbar />
-      <div style={{ height: 88 }} />
+      <DemoBanner />
 
       <React.Suspense>
         <Outlet />
