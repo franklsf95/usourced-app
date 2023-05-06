53feb53599d84831de4de7a33a33264af2d6b2d7
Frank Luan <lsf@berkeley.edu>
2023-05-06 00:07:41 +0000

    Team member bio dialog

--- app/routes/home/components/TeamSection.tsx
+++ app/routes/home/components/TeamSection.tsx
@@ -1,5 +1,16 @@
 import { LinkedIn } from "@mui/icons-material";
-import { Box, Container, Grid, Popover, Typography } from "@mui/material";
+import {
+  Box,
+  Container,
+  Dialog,
+  DialogContent,
+  Grid,
+  Slide,
+  Tooltip,
+  Typography,
+} from "@mui/material";
+import { decode } from "he";
+import { TransitionProps } from "notistack";
 import * as React from "react";
 import { useFirestoreImage } from "../../../core/firebase_utils.js";
 
@@ -14,25 +25,25 @@ const TEAM_MEMBERS = [
   {
     name: "Julia Xu",
     title: "Founder & CEO",
-    bio: "Julia also founded the omni-channel e-commerce lifestyle brand Multitasky (as seen in Good Morning America, Forbes, Cosmopolitan & more), where she gathered deep experience in the e-commerce and supply chain industry. She previously served as the Chief of Staff of Alibaba.com North America, where she led global operations and strategy. Before that, she was part of Disney&rsquo;s Corporate Strategy team, where she built the global financial model for Disney+. She also founded a McKinsey-Women&rsquo;s-Impact-Award-winning social enterprise Tink Knit. Julia holds a Bachelor of Business-Economics & Sociology dual degree from Brown University.",
+    bio: "Julia Xu is the CEO of USourced. She also founded the omni-channel e-commerce lifestyle brand Multitasky (as seen in Good Morning America, Forbes, Cosmopolitan & more), where she gathered deep experience in the e-commerce and supply chain industry. She previously served as the Chief of Staff of Alibaba.com North America, where she led global operations and strategy. Before that, she was part of Disney&rsquo;s Corporate Strategy team, where she built the global financial model for Disney+. She also founded a McKinsey-Women&rsquo;s-Impact-Award-winning social enterprise Tink Knit. Julia holds a Bachelor of Business-Economics & Sociology dual degree from Brown University.",
     linkedin_url: "https://www.linkedin.com/in/julialxu/",
   },
   {
     name: "Frank Luan",
     title: "Co-Founder & CTO",
-    bio: "Frank is also a final-year PhD student focused on AI systems at UC Berkeley. He has received 2 ACM best paper awards, and holds the current world record for CloudSort, a big data benchmark. Prior to that, he developed generative-AI-based programming assistants at Meta AI Research. He co-founded SketchMe in 2015, a social app company that received $25M venture funding. Frank holds a Bachelor of Science in Computer Science and Statistics dual degree from the University of Chicago.",
+    bio: "Frank Sifei Luan is the CTO of USourced. He is also a final-year PhD student focused on AI systems at UC Berkeley. He has received 2 ACM best paper awards, and holds the current world record for CloudSort, a big data benchmark. Prior to that, he developed generative-AI-based programming assistants at Meta AI Research. He co-founded SketchMe in 2015, a social app company that received $25M venture funding. Frank holds a Bachelor of Science in Computer Science and Statistics dual degree from the University of Chicago.",
     linkedin_url: "https://www.linkedin.com/in/franklsf95/",
   },
   {
     name: "Mandy Chow",
     title: "Co-Founder & COO",
-    bio: "Prior to joining USourced, Mandy was the Corporate Development Associate Director at Airwallex, where she led investments, and was also a core member of the fundraising team across its Series D, Series D2 Extension, and Series E ($5.5B valuation) rounds. Before that, she was in the TMT Investment Banking team at Goldman Sachs. Mandy holds a Bachelor of Science in Economics from The Wharton School of the University of Pennsylvania.",
+    bio: "Mandy Chow is the COO of USourced. Prior to joining USourced, Mandy was the Corporate Development Associate Director at Airwallex, where she led investments, and was also a core member of the fundraising team across its Series D, Series D2 Extension, and Series E ($5.5B valuation) rounds. Before that, she was in the TMT Investment Banking team at Goldman Sachs. Mandy holds a Bachelor of Science in Economics from The Wharton School of the University of Pennsylvania.",
     linkedin_url: "https://www.linkedin.com/in/mandy-chow-03463571/",
   },
   {
     name: "Frances Feng",
     title: "Co-Founder & CMO (Manufacturing)",
-    bio: "Frances has 12+ years of experience in the manufacturing industry. She previously held various project leadership roles at Cricut, where she developed new international vendor resources and implemented strategic initiatives that drove significant revenue growth (from $2M to $300M in 3 years). Frances holds a Bachelor of Business English from Guangdong University of Finances and Economics and is an active member of Project Management Professional (PMP).",
+    bio: "Frances Feng is the CMO (Manufacturing) of USourced. Frances has 12+ years of experience in the manufacturing industry. She previously held various project leadership roles at Cricut, where she developed new international vendor resources and implemented strategic initiatives that drove significant revenue growth (from $2M to $300M in 3 years). Frances holds a Bachelor of Business English from Guangdong University of Finances and Economics and is an active member of Project Management Professionals (PMP).",
     linkedin_url: "https://www.linkedin.com/in/frances-feng-b815896a/",
   },
   {
@@ -53,6 +64,7 @@ const TEAM_MEMBERS = [
   {
     name: "Corina Ivy Spi",
     title: "Content & Warehouse Manager",
+    bio: "Corina is the content and Warehouse Manager at USourced, boasting a proven track record in e-commerce and business development. Prior to her current role at USourced, she joined Multitasky in 2020 as an Operations Manager and Content Creator. Drawing on her experience from managing her own e-commerce store, Corina has honed her industry expertise, demonstrating versatility and a commitment to excellence. Her hands-on experience and entrepreneurial spirit continue to contribute significantly to the growth and success of both companies.",
   },
   {
     name: "Sabrina Feng",
@@ -89,85 +101,131 @@ const TEAM_MEMBERS = [
 const DEFAULT_PROFILE_IMAGE_URL = "/home/silhouette.png";
 
 function TeamMemberCard({ member }: { member: MemberProfile }) {
-  const [anchorEl, setAnchorEl] = React.useState<HTMLElement | null>(null);
   const { image } = useFirestoreImage({
     path: `/usourced/team/${member.name.split(" ")[0]}.jpg`,
   });
-
-  const handlePopoverOpen = (event: React.MouseEvent<HTMLElement>) => {
-    setAnchorEl(event.currentTarget);
+  const [open, setOpen] = React.useState(false);
+  const handleOpen = () => {
+    setOpen(true);
   };
-
-  const handlePopoverClose = () => {
-    setAnchorEl(null);
+  const handleClose = () => {
+    setOpen(false);
   };
-
-  const open = Boolean(anchorEl);
-
-  const profileEl = (
-    <Box
-      component="img"
-      src={image ?? DEFAULT_PROFILE_IMAGE_URL}
-      sx={{ height: 160, maxWidth: 160, borderRadius: 80 }}
-    />
-  );
-
-  const wrappedProfileEl = member.bio ? (
-    <>
-      <Box onMouseEnter={handlePopoverOpen} onMouseLeave={handlePopoverClose}>
-        {profileEl}
-      </Box>
-      <Popover
-        sx={{
-          pointerEvents: "none",
-        }}
-        open={open}
-        anchorEl={anchorEl}
-        anchorOrigin={{
-          vertical: "center",
-          horizontal: "center",
-        }}
-        transformOrigin={{
-          vertical: "top",
-          horizontal: "left",
-        }}
-        onClose={handlePopoverClose}
-        disableRestoreFocus
-      >
-        <Box width={400}>
-          <Typography variant="body1" paragraph sx={{ m: 2 }}>
-            {member.bio}
-          </Typography>
-        </Box>
-      </Popover>
-    </>
-  ) : (
-    profileEl
-  );
-
   return (
     <Grid item xs={12} sm={6} md={3} key={member.name}>
-      <Box>
-        {wrappedProfileEl}
-        <Typography variant="h5">{member.name}</Typography>
-        <Typography variant="body2" color="#6D7F83">
-          {member.title}{" "}
-          {member.linkedin_url && (
-            <a
-              href={member.linkedin_url}
-              style={{
-                display: "block-inline",
-                position: "relative",
-                bottom: -8,
-                color: "inherit",
-              }}
-            >
-              <LinkedIn />
-            </a>
-          )}
-        </Typography>
-      </Box>
+      <Tooltip title="Click to view bio" placement="top" arrow>
+        <Box
+          onClick={handleOpen}
+          sx={{
+            cursor: "pointer",
+            "&:hover": {
+              animation: "pulse 0.5s",
+            },
+          }}
+        >
+          <Box
+            component="img"
+            src={image ?? DEFAULT_PROFILE_IMAGE_URL}
+            sx={{ height: 160, maxWidth: 160, borderRadius: 80 }}
+          />
+          <Typography variant="h5">{member.name}</Typography>
+          <Typography variant="body2" color="#6D7F83">
+            {member.title}
+          </Typography>
+        </Box>
+      </Tooltip>
+      <TeamMemberDetailDialog
+        member={member}
+        memberImage={image}
+        open={open}
+        handleClose={handleClose}
+      />
     </Grid>
+  );
+}
+
+const Transition = React.forwardRef(function Transition(
+  props: TransitionProps & {
+    children: React.ReactElement;
+  },
+  ref: React.Ref<unknown>,
+) {
+  return <Slide direction="left" ref={ref} {...props} />;
+});
+
+function TeamMemberDetailDialog({
+  member,
+  memberImage,
+  open,
+  handleClose,
+}: {
+  member: MemberProfile;
+  memberImage: string | null;
+  open: boolean;
+  handleClose: () => void;
+}): JSX.Element {
+  return (
+    <Dialog
+      open={open}
+      PaperProps={{
+        sx: {
+          position: "fixed",
+          bottom: 0,
+          right: 0,
+          maxWidth: "sm",
+          height: "100vh",
+        },
+      }}
+      TransitionComponent={Transition}
+      keepMounted
+      onClose={handleClose}
+    >
+      <DialogContent
+        sx={{
+          textAlign: "center",
+          pt: 10,
+          backgroundImage: "url(/home/about-background.svg)",
+          backgroundPosition: "bottom",
+          backgroundSize: "cover",
+          backgroundRepeat: "no-repeat",
+        }}
+      >
+        <Box
+          component="img"
+          src={memberImage ?? DEFAULT_PROFILE_IMAGE_URL}
+          sx={{ height: 160, maxWidth: 160, borderRadius: 80 }}
+        />
+        <Typography variant="h2">{member.name}</Typography>
+        <Typography variant="body2" color="#6D7F83">
+          {member.title}
+        </Typography>
+        {member.bio && (
+          <Box mt={4} px={4}>
+            <Typography
+              variant="body1"
+              textAlign="left"
+              borderTop="1px solid #ccc"
+              pt={2}
+            >
+              {decode(member.bio)}
+            </Typography>
+          </Box>
+        )}
+        {member.linkedin_url && (
+          <a
+            href={member.linkedin_url}
+            style={{
+              display: "block-inline",
+              position: "relative",
+              bottom: -8,
+              color: "inherit",
+            }}
+          >
+            <LinkedIn />
+          </a>
+        )}
+      </DialogContent>
+    </Dialog>
   );
 }
 
